813ced19a0f130604b3023bd5b8114be41241069
Daniel Minshew <ofenixculpa@gmail.com>
2016-06-16 09:16:56 -0500

    add privacy policy and terms modals

--- js/init.js
+++ js/init.js
@@ -8,5 +8,11 @@
       selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 
+    $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal();
+    });
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
