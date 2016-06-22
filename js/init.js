c617da6d153a976abfbdc474727b0fc0daebb61c
Dan Minshew <ofenixculpa@gmail.com>
2016-06-22 09:05:48 -0500

    merging to master for more readable urls for static files (#1)
    * add recaptcha gif and date picker
    
    * few style changes
    
    * update styling
    
    * Add reCaptcha and fixed checkbox validation
    
    * style recaptcha
    
    * styling
    
    * match fields to custom vtiger fields in Leads module
    
    * fixed checkboxes
    
    * add privacy policy and terms modals
    
    * style tweaks :sunglasses:
    
    * style changes
    
    * change the color of teal
    
    * dealer app font color
    
    * add a favicon
    
    * little styling things
    
    * starting a require recaptcha script
    
    * add recaptcha test
    
    * try again
    
    * require recatchpa
    
    * recaptcha now renders properly
    
    * prevent default
    
    * checking response
    
    * this should do it
    
    * it works\!
    
    * all fields comply with Leads schema, add button to copy legal name into dba field
    
    * styling and require fields changes
    
    * remove one break
    
    * fix stupid text copy thing
    
    * basically done
    
    * trying to fix broken radio button
    
    * add default values
    
    * and remove value field..
    
    * changed to select
    
    * fixed everything
    
    * cleanup and add cname
    
    * remove cname temporarily
    
    * add cname record again
    
    * add dealer agreement
    
    * add range sliders for percentage fields
    
    * add separate pages, prep for agreement/terms pdfs, add success page

--- js/init.js
+++ js/init.js
@@ -3,5 +3,29 @@
 
     $('.button-collapse').sideNav();
 
+    $('._todays_date').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year
+    });
+
+    $(document).ready(function() {
+      $('#dba_select').material_select();
+    });
+
+    $(document).ready(function(){
+      // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal();
+
+      $("#dba_same").change(function() {
+        if ($('#dba_same').prop('checked')) {
+            var existing = $('#legal_name').val()
+            $('#dba').val(existing)
+        }
+        if ($('#dba').val() == "") {
+          $('#company_label').removeClass('active')
+        }
+        $('#company_label').addClass('active')
+      });
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
---
+++ js/vtiger_post.js
@@ -0,0 +1,9 @@
+/*
+
+Requirements
+
+. query DOM for appropriate fields
+. match to vTiger 'Lead' schema
+. post via http in a form digestable by vTiger's backend
+
+*/
