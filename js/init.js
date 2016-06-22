6fcb186291cdc462849f029bcb41f4a168568995
Daniel Minshew <ofenixculpa@gmail.com>
2016-06-22 15:09:40 -0500

    same proper date range, update todos

--- js/init.js
+++ js/init.js
@@ -5,7 +5,8 @@
 
     $('._todays_date').pickadate({
       selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15, // Creates a dropdown of 15 years to control year
+      selectYears: 100, // Creates a dropdown of 15 years to control year
+      max: true,
     });
 
     $(document).ready(function() {
